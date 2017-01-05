bd53f7d6ef86ff7a6538527c5a7e26d374a95a37
tle <toan.lee@outlook.com>
2017-01-05 11:46:35 +0700

    Finish part 1 of tutorial on Token Based Authencation on API Adding Web (FrontEnd)

---
+++ AngularJSAuthentication.API/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using AngularJSAuthentication.API.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+
+namespace AngularJSAuthentication.API.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// Responsible for validating the “Client”, 
+        /// In our case we have only one client so we’ll always return that its validated successfully
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        /// <summary>
+        /// Responsible to validate the username and password sent to the authorization server’s token endpoint
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            /*
+             To allow CORS on the token middleware provider we need to add the header “Access-Control-Allow-Origin” to Owin context, 
+             if you forget this, generating the token will fail when you try to call it from your browser. 
+             Not that this allows CORS for token middleware provider not for ASP.NET Web API which we’ll add on the next step.
+             */
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            // Use the “AuthRepository” class we created earlier and call the method “FindUser” to check if the username and password are valid.
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            /*
+             If the credentials are valid we’ll create “ClaimsIdentity” class and pass the authentication type to it, 
+             in our case “bearer token”, then we’ll add two claims (“sub”,”role”) and those will be included in the signed token. 
+             You can add different claims here but the token size will increase for sure.
+             */
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            // Now generating the token happens behind the scenes when we call “context.Validated(identity)”.
+            context.Validated(identity);
+        }
+    }
+}
--- AngularJSAuthentication.API/Startup.cs
+++ AngularJSAuthentication.API/Startup.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-using Microsoft.Owin;
 using Microsoft.Owin;
 using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
+using AngularJSAuthentication.API.Providers;
+using Microsoft.Owin.Security.OAuth;
 
 [assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
 namespace AngularJSAuthentication.API
@@ -29,11 +22,39 @@ namespace AngularJSAuthentication.API
         /// </param>
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
+
             // The “HttpConfiguration” object is used to configure API routes, so we’ll pass this object to method “Register” in “WebApiConfig” class.
             HttpConfiguration config = new HttpConfiguration();
-            //Lastly, we’ll pass the “config” object to the extension method “UseWebApi” which will be responsible to wire up ASP.NET Web API to our Owin server pipeline.
+            
+            // Lastly, we’ll pass the “config” object to the extension method “UseWebApi” which will be responsible to wire up ASP.NET Web API to our Owin server pipeline.
             WebApiConfig.Register(config);
+
+            // Allow CORS for ASP.NET Web API
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                
+                // The path for generating tokens will be as :”http://localhost:port/token”. We’ll see how we will issue HTTP POST request to generate token in the next steps.
+                TokenEndpointPath = new PathString("/token"),
+                
+                // We’ve specified the expiry for token to be 24 hours, so if the user tried to use the same token for authentication after 24 hours from the issue time, his request will be rejected and HTTP status code 401 is returned.
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1), // Test on 1 minute
+
+                // We’ve specified the implementation on how to validate the credentials for users asking for tokens in custom class named “SimpleAuthorizationServerProvider”.
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
 
     }
