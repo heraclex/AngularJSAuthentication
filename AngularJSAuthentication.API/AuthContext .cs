08ffa919c38714922eae3531ebbaf687bacc1ca7
toanle <toanlee@outlook.com>
2017-02-07 21:44:24 +0700

    update build at home

--- AngularJSAuthentication.API/AuthContext .cs
+++ AngularJSAuthentication.API/AuthContext .cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using AngularJSAuthentication.API.Entities;
 
 namespace AngularJSAuthentication.API
 {
@@ -16,8 +17,8 @@ namespace AngularJSAuthentication.API
 
         }
 
-        //public DbSet<Client> Clients { get; set; }
-        //public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 
 }
