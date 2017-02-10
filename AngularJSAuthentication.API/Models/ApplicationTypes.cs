5991c94ac181362c265da2ff8fde207b49371051
toanle <toanlee@outlook.com>
2017-02-10 08:05:35 +0700

    adding comment

--- AngularJSAuthentication.API/Models/ApplicationTypes.cs
+++ AngularJSAuthentication.API/Models/ApplicationTypes.cs
@@ -7,6 +7,9 @@ namespace AngularJSAuthentication.API.Models
 {
     public enum ApplicationTypes
     {
+        /*In our case I’ve identified clients to two types (JavaScript – Nonconfidential) and (Native-Confidential) 
+         * which means that for confidential clients  we can store the client secret in confidential way (valid for desktop apps, mobile apps, server side web apps) 
+         * so any request coming from this client asking for access token should include the client id and secret.*/
         JavaScript = 0,
         NativeConfidential = 1
     };
